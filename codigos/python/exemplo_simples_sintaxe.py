c4edb9a3a27d8949a177d6e9793c5c70fedeb5c8
Lucas Boaventura <lucasxboaventura@hotmail.com>
2025-10-22 12:30:08 -0300

    adiciona exemplo simples de oo com python

--- codigos/python/exemplo_simples_sintaxe.py
+++ codigos/python/exemplo_simples_sintaxe.py
@@ -1,7 +1,24 @@
-pessoa = {'nome': 'Lucas', 'idade': 25} 
+class Cachorro:
+    def __init__(self, nome, raca = 'Vira-Lata'):
+        self.__nome = nome
+        self.raca = raca
+        print(f'O cachorro {self.__nome} foi criado!')
 
-for chave, valor in pessoa:
-    print(chave, ' - ', valor)
+    def set_nome(self, nome):
+        self.__nome = nome
 
-print('Acabou')
+    def get_nome(self):
+        return self.__nome
 
+    def latir(self):
+        return f'{self.__nome} da raca {self.raca} esta latindo!' 
+
+meu_cachorro = Cachorro("Léo")
+outro_cachorro = Cachorro("Lyon", "Chow-Chow")
+
+print(meu_cachorro.latir())
+print(outro_cachorro.latir())
+
+meu_cachorro.set_nome("Teste")
+print(meu_cachorro.get_nome())
+print(meu_cachorro.latir())
